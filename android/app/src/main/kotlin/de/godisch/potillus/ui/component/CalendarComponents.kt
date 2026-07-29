110231b158061f99456cc7cfa36fd9f731502883
Martin A. Godisch <android@godisch.de>
2026-07-29 18:07:59 +0200

    Drop the unused semantics role import
    Signed-off-by: Martin A. Godisch <android@godisch.de>

--- android/app/src/main/kotlin/de/godisch/potillus/ui/component/CalendarComponents.kt
+++ android/app/src/main/kotlin/de/godisch/potillus/ui/component/CalendarComponents.kt
@@ -59,7 +59,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import de.godisch.potillus.R
