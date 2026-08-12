8de2df693e8bd872006e7bad345687c72b8ec74d
Martin A. Godisch <android@godisch.de>
2026-08-12 19:41:07 +0200

    Import semantics in Components.kt
    Signed-off-by: Martin A. Godisch <android@godisch.de>

--- android/app/src/main/kotlin/de/godisch/potillus/ui/component/Components.kt
+++ android/app/src/main/kotlin/de/godisch/potillus/ui/component/Components.kt
@@ -61,6 +61,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
