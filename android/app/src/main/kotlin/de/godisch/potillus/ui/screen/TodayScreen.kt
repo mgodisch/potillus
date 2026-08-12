f328cb3ecc1b8f5af6548dd13f07934c4f30cf7c
Martin A. Godisch <android@godisch.de>
2026-08-12 19:13:37 +0200

    Sort imports in TodayScreen.kt
    Signed-off-by: Martin A. Godisch <android@godisch.de>

--- android/app/src/main/kotlin/de/godisch/potillus/ui/screen/TodayScreen.kt
+++ android/app/src/main/kotlin/de/godisch/potillus/ui/screen/TodayScreen.kt
@@ -36,9 +36,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
